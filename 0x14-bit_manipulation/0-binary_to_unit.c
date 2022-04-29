822ee933b308e19f216b5a2b26e32e875851d0fb
Oraclevirus77 <nwekemichael53@gmail.com>
2022-04-29 15:56:56 +0000

    Wahala nor dey finish for programming

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -1,37 +1,26 @@
 #include "main.h"
-
+#include <math.h>
+#include <string.h>
 /**
- * binary_to_uint - converts a binary number to an
- * unsigned int.
- * @b: binary.
- *
- * Return: unsigned int.
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: char
+ * Return: converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	unsigned int len, sum = 0, i;
 
-	if (!b)
-		return (0);
-
-	ui = 0;
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	len = strlen(b);
+	i = len - 1;
+	while (*b != '\0')
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b == NULL || (*b != '0') || (*b != '1'))
 		{
 			return (0);
 		}
-
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
+		sum += (int)(*b) * pow(2, i);
+		i--;
+		b++;
 	}
-
-	return (ui);
+	return (sum);
 }
